bdd429acc8fe7705972f0ca240f3b2de06b083c0
klieolee <105428181+klieolee@users.noreply.github.com>
2022-05-17 18:34:48 +0800

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -28,8 +28,8 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+X = iris.drop('species'),axis=1)
+Y = iris.species
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,7 +38,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(['Iris-setosa',')
 
 st.subheader('Prediction')
 st.write(iris.target_names[prediction])
