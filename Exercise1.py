9614372a9e9cd87d8ab8e01f360155cb80a9cea6
klieolee <105428181+klieolee@users.noreply.github.com>
2022-05-17 18:43:11 +0800

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -27,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
+iris = pd.read_cvs('https://raw.githubusercontent.com/klieolee/SDEC-Coursework/main/IRIS.csv')
 X = iris.drop('species'),axis=1)
 Y = iris.species
 
